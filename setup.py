31fb9b1428e293e3f0ebe98183ddc2df02f5bfff
sikander <sikander@freyaquaponics.com>
2018-11-05 19:56:42 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#from distutils.core import setup
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='Dice-Roller',
+    version='1.8',
+    license='GPL3',
+    author = 'Victor Roest',
+    author_email = 'victor@xirion.net',
+    url = 'https://gitlab.xirion.net/vroest/dice-roller',
+    download_url = 'https://gitlab.xirion.net/vroest/dice-roller/repository/v1.8/archive.tar.gz',
+    keywords = ['dnd', 'dice'],
+    description= 'A dice-rolling application using standard dice notation',
+    long_description=long_description,
+    scripts=[
+        'roll',
+    ]
+)
